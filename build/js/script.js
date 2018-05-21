c57c89cb0abae8f5d50d7546c63f80afa90614a2
Nickolay <36889136+johnywinter@users.noreply.github.com>
2018-05-21 18:47:06 +0300

    исправления ч12

--- build/js/script.js
+++
@@ -1,6 +0,0 @@
-var toggleBtn = document.querySelector(".header__menu-toggle");
-var toggleMenu = document.querySelector(".menu");
-
-  toggleBtn.addEventListener("click", function () {
-    toggleMenu.classList.toggle("menu--open");
-  });
